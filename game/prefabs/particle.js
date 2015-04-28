d5de61ec57a3ee021a8e75c722a9531dbbe978ea
Marvin Qian <fueledbymarvin@gmail.com>
2015-04-27 20:02:08 -0400

    Add basic particle drawing

--- game/prefabs/particle.js
+++ game/prefabs/particle.js
@@ -1,10 +1,20 @@
 'use strict';
 
-var Particle = function(game, x, y, material) {
+var Particle = function(game, x, y, player, material, myCollisionGroup, otherCollisionGroup) {
     Phaser.Sprite.call(this, game, x, y, 'circle');
 
-    this.game.physics.p2.enable(this);
+    this.game.physics.p2.enable(this, false);
+
+    if (player === "player1")
+	this.tint = 0xFF0000;
+    else
+	this.tint = 0x00FF00;
+
     this.body.setMaterial(material);
+    this.body.setCircle(8);
+    this.body.setCollisionGroup(myCollisionGroup);
+    this.body.collides(otherCollisionGroup, this.collideOpponent, this);
+    this.body.collides(myCollisionGroup);
 };
 
 Particle.prototype = Object.create(Phaser.Sprite.prototype);
@@ -15,5 +25,9 @@ Particle.prototype.update = function() {
   // write your prefab's specific update code here
   
 };
+
+Particle.prototype.collideOpponent = function(body1, body2) {
+    console.log("collide");
+}
 
 module.exports = Particle;
--- game/states/play.js
+++ game/states/play.js
@@ -7,6 +7,7 @@ Play.prototype = {
     create: function() {
 	this.game.physics.startSystem(Phaser.Physics.P2JS);
 
+	this.game.physics.p2.setImpactEvents(true);
 	this.game.physics.p2.gravity.y = 300;
 
 	this.spriteMaterial = this.game.physics.p2.createMaterial('spriteMaterial');
@@ -15,13 +16,20 @@ Play.prototype = {
 
 	this.game.physics.p2.setWorldMaterial(this.worldMaterial);
 
+	this.player1CG = this.game.physics.p2.createCollisionGroup();
+	this.player2CG = this.game.physics.p2.createCollisionGroup();
+	this.game.physics.p2.updateBoundsCollisionGroup();
+
 	this.particles = this.game.add.group();
-	for (var i = 0; i < 100; i++) {
-	    this.particles.add(new Particle(this.game, Math.floor(800*Math.random()), Math.floor(800*Math.random()), this.spriteMaterial));
-	}
+	this.shift = this.game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
     },
     update: function() {
-
+	if (this.game.input.mousePointer.isDown) {
+	    if (this.shift.isDown)
+		this.particles.add(new Particle(this.game, this.game.input.mousePointer.position.x, this.game.input.mousePointer.position.y, "player1", this.spriteMaterial, this.player1CG, this.player2CG));
+	    else
+		this.particles.add(new Particle(this.game, this.game.input.mousePointer.position.x, this.game.input.mousePointer.position.y, "player2", this.spriteMaterial, this.player2CG, this.player1CG));
+	}
     },
 };
 
